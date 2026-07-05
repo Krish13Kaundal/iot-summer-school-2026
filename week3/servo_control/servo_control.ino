66fc9844a483ae56eb09e46eb05e638fb7c6fae2
Krish13Kaundal <krish1308august2006@gmail.com>
2026-07-05 20:51:11 +0530

    Update servo_control.ino

--- week3/servo_control/servo_control.ino
+++ week3/servo_control/servo_control.ino
@@ -1,52 +1,34 @@
-const int ENABLE_PIN = 9; // PWM pin for Speed Control (Pin 1 of L293D)
-const int INPUT1 = 7;     // Direction Control Pin (Pin 2 of L293D)
-const int INPUT2 = 8;     // Direction Control Pin (Pin 7 of L293D)
-const int BTN_DIR = 2;
-const int BTN_STOP = 3;
-const int POT_PIN = A0;
+#include <Servo.h>
 
-bool isForward = true;
-bool isRunning = true;
+Servo myServo;
+const int POT_PIN = A0;
+const int SWEEP_BUTTON = 3;
+const int SERVO_PIN = 6;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ENABLE_PIN, OUTPUT);
-  pinMode(INPUT1, OUTPUT);
-  pinMode(INPUT2, OUTPUT);
-  pinMode(BTN_DIR, INPUT_PULLUP);
-  pinMode(BTN_STOP, INPUT_PULLUP);
+  myServo.attach(SERVO_PIN);
+  pinMode(SWEEP_BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
-  if (digitalRead(BTN_STOP) == LOW) {
-    isRunning = !isRunning;
-    delay(300); // Debounce
-  }
-
-  if (digitalRead(BTN_DIR) == LOW) {
-    isForward = !isForward;
-    delay(300); // Debounce
-  }
-
-  int potValue = analogRead(POT_PIN);
-  int pwmSpeed = map(potValue, 0, 1023, 0, 255);
-  int speedPct = map(pwmSpeed, 0, 255, 0, 100);
-
-  if (isRunning) {
-    analogWrite(ENABLE_PIN, pwmSpeed); // Control speed via PWM
-    if (isForward) {
-      digitalWrite(INPUT1, HIGH);
-      digitalWrite(INPUT2, LOW);
-    } else {
-      digitalWrite(INPUT1, LOW);
-      digitalWrite(INPUT2, HIGH);
+  if (digitalRead(SWEEP_BUTTON) == LOW) {
+    Serial.println("Executing Full Sweep (0 -> 180 -> 0)");
+    for (int pos = 0; pos <= 180; pos++) {
+      myServo.write(pos);
+      delay(15);
     }
+    for (int pos = 180; pos >= 0; pos--) {
+      myServo.write(pos);
+      delay(15);
+    }
+    delay(500); // Debounce
   } else {
-    analogWrite(ENABLE_PIN, 0); // Stop motor
+    int potVal = analogRead(POT_PIN);
+    int angle = map(potVal, 0, 1023, 0, 180);
+    myServo.write(angle);
+    Serial.print("Servo Angle: ");
+    Serial.println(angle);
+    delay(100);
   }
-
-  Serial.print("State: "); Serial.print(isRunning ? "RUNNING" : "STOPPED");
-  Serial.print(" | Direction: "); Serial.print(isForward ? "FORWARD" : "REVERSE");
-  Serial.print(" | Speed: "); Serial.print(speedPct); Serial.println("%");
-  delay(300);
 }
